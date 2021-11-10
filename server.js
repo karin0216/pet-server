05908e06439aa338ed145e98202b18e08da8ce38
yutakaki <yu.takaki2000@gmail.com>
2021-11-10 15:15:36 +0900

    setup reactRouterDown in app.jsx

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const Socket = require("socket.io");
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
 	console.log(`listening to port ${port}`);
+});
+
+const io = Socket(server);
+
+io.socket.on("connection", (socket) => {
+	console.log(socket.io);
 });
