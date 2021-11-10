d5b6319b32bcc4b7626765efc5e30f9fd3d795aa
karin0216 <cheer.karin0216@gmail.com>
2021-11-10 16:41:42 +0900

    create User schema

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6
+    },
+    description: {
+        type: String,
+        default: ""
+    },
+    profile_picture: {
+        type: String,
+        default: ""
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
