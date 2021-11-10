2a219a581aeb892064372021ce2214de685ab49c
karin0216 <cheer.karin0216@gmail.com>
2021-11-10 16:30:59 +0900

    configure mongoose

--- db.js
+++ db.js
@@ -1,1 +1,19 @@
-// configure mongodb
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", err => {
+    console.log("Mongoose connection error:" + err.messsage);
+});
+
+db.once('open', () => {
+    console.log("MongoDB Connected!");
+});
+
+module.exports = db;
