a976921101facd357a31e1965cfe930a439eb5ae
yutakaki <yu.takaki2000@gmail.com>
2021-11-10 21:20:33 +0900

    feat: change cors origin n socket io

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const Socket = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 
 const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.get("/test", (req, res) => {
+	res.send("hello");
+});
 const server = app.listen(port, () => {
 	console.log(`listening to port ${port}`);
 });
 
 const io = Socket(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "*",
 	},
 });
 
