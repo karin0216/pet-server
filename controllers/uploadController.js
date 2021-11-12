3e5c536c09446199916303b40dcbc14531e81075
karin0216 <cheer.karin0216@gmail.com>
2021-11-12 16:37:34 +0900

    continue uploading

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,4 +1,5 @@
 const upload = require('../middleware/upload');
+const Picture = require('../models/Picture');
 require('dotenv').config();
 
 const MongoClient = require('mongodb').MongoClient;
@@ -8,19 +9,19 @@ const url = process.env.MONGO_URL;
 
 //const mongoClient = new MongoClient(url);
 
-
-
-
 const uploadFiles = async (req, res) => {
     try {
         await upload(req, res);
+        const newPic = new Picture(req.file);
+        const savedPic = await newPic.save();
         console.log(req.file);
 
         if (req.file === undefined) {
             return res.send('You must select a file.')
         }
-
-        return res.send("File has been uploaded!");
+        
+        return res.status(200).send(savedPic);
+        //return res.send("File has been uploaded!");
 
     } catch (err) {
         console.log(err);
