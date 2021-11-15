f9c3ed8aaff44f9524c128fe9396f5739101d7a0
karin0216 <cheer.karin0216@gmail.com>
2021-11-15 12:32:28 +0900

    create functionality of uploading multiple images

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,29 +1,30 @@
-const util = require('util');
-const multer = require('multer');
-const { GridFsStorage } = require('multer-gridfs-storage');
-require('dotenv').config();
+const util = require("util");
+const multer = require("multer");
+const { GridFsStorage } = require("multer-gridfs-storage");
+require("dotenv").config();
 
 const storage = new GridFsStorage({
-    url: process.env.MONGO_URL,
-    options: {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    },
-    file: (req, file) => {
-        const match = ["image/png", "image/jpeg"];
+  url: process.env.MONGO_URL,
+  options: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  file: (req, file) => {
+    const match = ["image/png", "image/jpeg"];
 
-        if (match.indexOf(file.mimetype) === -1) {
-            const filename = `${Date.now()}-petapp-${file.originalname}`;
-            return filename;
-        }
-
-        return {
-            bucketName: "photos",
-            filename: `${Date.now()}-petapp-${file.originalname}`
-        };
+    if (match.indexOf(file.mimetype) === -1) {
+      const filename = `${Date.now()}-petapp-${file.originalname}`;
+      return filename;
     }
+
+    return {
+      bucketName: "photos",
+      filename: `${Date.now()}-petapp-${file.originalname}`,
+    };
+  },
 });
 
-const uploadFile = multer({ storage }).single('file');
-const uploadFilesMiddleware = util.promisify(uploadFile);
+//const uploadFile = multer({ storage }).single('file');
+const uploadFiles = multer({ storage }).array("file", 5);
+const uploadFilesMiddleware = util.promisify(uploadFiles);
 module.exports = uploadFilesMiddleware;
