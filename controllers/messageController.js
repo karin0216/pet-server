f2cd13e51ad0f9678fccc2ebed55d792782493d9
yutakaki <yu.takaki2000@gmail.com>
2021-11-15 10:56:28 +0900

    merge

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -4,7 +4,7 @@ const Message = require("../models/Message");
 const addConversation = async (req, res) => {
 	try {
 		//sampple
-		const members = ["61907b4ab2074915baac9cb0", "61907bafb2074915baac9cba"];
+		const members = ["61907b4ab2074915baac9cb0", "61907ec10db74d6c4d0e7cb6"];
 		const checkIfExist = await Conversation.find({
 			members: { $all: members },
 		});
@@ -14,7 +14,7 @@ const addConversation = async (req, res) => {
 					userId: "61907b4ab2074915baac9cb0",
 				},
 				{
-					userId: "61907bafb2074915baac9cba",
+					userId: "61907ec10db74d6c4d0e7cb6",
 				},
 			];
 			const createAction = await Conversation.create({ members, seen });
