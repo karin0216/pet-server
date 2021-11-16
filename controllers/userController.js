ced5aae3566f4b0cd21de472e9cc974fa2377d43
Eiko0705 <eiko.imai.070502@gmail.com>
2021-11-16 11:10:59 +0900

    feat: changeing the way to upload the data in pet controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,33 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const { update } = require("../models/User");
 
 // update user info
 const updateUser = async (req, res) => {
-	if (req.body.id === req.params.id) {
+	const request_data = req.body.data;
+	const request_id = req.body._id;
+  console.log(request_id);
+  console.log(req.params.id);
+
+	if (request_id === req.params.id) {
 		// if a user want to change password we need to hash it again
-		if (req.body.password) {
+		if (request_data.password) {
 			try {
 				const salt = await bcrypt.genSalt(10);
-				req.body.password = await bcrypt.hash(req.body.password, salt);
+				request_data.password = await bcrypt.hash(request_data.password, salt);
 			} catch (err) {
 				res.status(500).send(err);
 			}
 		}
 	}
 	try {
-		await User.findByIdAndUpdate(req.params.id, {
-			$set: req.body,
-		});
-		res.status(200).send("Account is successfully updated!");
+		console.log("Helloooooooo");
+		console.log(request_data);
+		const result = await User.updateOne({ _id: req.params.id }, { $set: {'email': 'test1@gmail.com'} });
+		console.log(result);
+		res.status(200).send(user);
+		// console.log(user);
+	
 	} catch (err) {
 		res.status(500).send(err);
 	}
