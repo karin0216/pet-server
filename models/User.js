3aacd622c5ac2cead0a53cdc0b651c5d1f347188
karin0216 <cheer.karin0216@gmail.com>
2021-11-19 16:53:54 +0900

    created tag schema , add tag field to pet model and add interest field to user model

--- models/User.js
+++ models/User.js
@@ -3,34 +3,37 @@ const CarerSchema = require("./Carer");
 const OwnerSchema = require("./Owner");
 
 const UserSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-		min: 6,
-	},
-	description: {
-		type: String,
-		default: "",
-	},
-	profile_picture: {
-		type: String,
-		default: "",
-	},
-	type: {
-		type: String,
-		required: true,
-	},
-	Carer: CarerSchema,
-	Owner: OwnerSchema,
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  profile_picture: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  interest: {
+    type: String,
+  },
+  Carer: CarerSchema,
+  Owner: OwnerSchema,
 });
 
 module.exports = mongoose.model("User", UserSchema, "Users");
