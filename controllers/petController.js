71c47cede20ceacbb0489c1aff13db49fb02b109
karin0216 <cheer.karin0216@gmail.com>
2021-11-20 14:16:43 +0900

    fxied typo

--- controllers/petController.js
+++ controllers/petController.js
@@ -2,100 +2,101 @@ const Pet = require("../models/Pet");
 
 //get all pets
 const getAllPets = async (req, res) => {
-	try {
-		const pets = await Pet.find();
-		res.status(200).send(pets);
-	} catch (err) {
-		console.log(err);
-		res.status(500).send(err);
-	}
+  try {
+    const pets = await Pet.find();
+    res.status(200).send(pets);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
 
 // add pet info
 const addPet = async (req, res) => {
-	try {
-		const { name, description, pet_pictures, type, owner_id, questionnaire } = req.body;
-		const savedPet = await Pet.create({
-			name: name,
-			description: description,
-			pet_pictures: pet_pictures,
-			type: type,
-			owner_id: owner_id,
-			questionnaire: questionnaire,
-		});
-		res.status(200).send(savedPet);
-	} catch (err) {
-		console.log(err);
-		res.status(500).send(err);
-	}
+  try {
+    const { name, description, pet_pictures, type, owner_id, questionnaire } =
+      req.body;
+    const savedPet = await Pet.create({
+      name: name,
+      description: description,
+      pet_pictures: pet_pictures,
+      type: type,
+      owner_id: owner_id,
+      questionnaire: questionnaire,
+    });
+    res.status(200).send(savedPet);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
 
 // get pet info
 const getPet = async (req, res) => {
-	try {
-		const pet = await Pet.findById(req.params.id);
-		res.status(200).send(pet);
-	} catch (err) {
-		console.log(err);
-		res.status(500).send(err);
-	}
+  try {
+    const pet = await Pet.findById(req.params.id);
+    res.status(200).send(pet);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
 
-// get pet info by ower_id
+// get pet info by owner_id
 const getPetByOwnerId = async (req, res) => {
-	try {
-		const { user_id } = req.user;
-		const pet = await Pet.findOne({ owner_id: user_id });
-		res.status(200).send(pet);
-	} catch (err) {
-		console.log(err);
-		res.status(500).send(err);
-	}
+  try {
+    const { user_id } = req.user;
+    const pet = await Pet.findOne({ owner_id: user_id });
+    res.status(200).send(pet);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
 
 // get pets by pet type
 const getPetsByType = async (req, res) => {
-	try {
-		const pets = await Pet.find({ type: req.params.type.toLowerCase() });
-		res.status(200).send(pets);
-	} catch (err) {
-		console.log(err);
-		res.status(500).send(err);
-	}
+  try {
+    const pets = await Pet.find({ type: req.params.type.toLowerCase() });
+    res.status(200).send(pets);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
 
 // update pet info
 const updatePet = async (req, res) => {
-	try {
-		await Pet.findByIdAndUpdate(req.params.id, {
-			$set: req.body,
-		});
-		res.status(200).send("Pet info is successfully updated!");
-	} catch (err) {
-		console.log(err);
-		res.status(500).send(err);
-	}
+  try {
+    await Pet.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    res.status(200).send("Pet info is successfully updated!");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
 
 // delete pet info
 const deletePet = async (req, res) => {
-	try {
-		await Pet.findByIdAndDelete(req.params.id);
-		res.status(200).send("Pet info is successfully deleted!");
-	} catch (err) {
-		console.log(err);
-		res.status(500).send(err);
-	}
+  try {
+    await Pet.findByIdAndDelete(req.params.id);
+    res.status(200).send("Pet info is successfully deleted!");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
 
 // get all requests for pet
 
 module.exports = {
-	addPet,
-	getPet,
-	getPetByOwnerId,
-	getPetsByType,
-	updatePet,
-	deletePet,
-	getAllPets,
+  addPet,
+  getPet,
+  getPetByOwnerId,
+  getPetsByType,
+  updatePet,
+  deletePet,
+  getAllPets,
 };
